e95f984f9ceab3aea2f6aa50086eb2e27cdcf726
Huy Phạm <huypd303@gmail.com>
2023-11-03 21:07:45 +0700

    update

--- src/test/java/Locators/LocatorsCRM.java
+++ src/test/java/Locators/LocatorsCRM.java
@@ -18,13 +18,18 @@ public class LocatorsCRM {
         webDriver.get("https://crm.anhtester.com/admin/authentication");
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 
-        webDriver.findElement(By.id("email")).sendKeys("admin@example.com");
+        //email
+        //webDriver.findElement(By.id("email")).sendKeys("admin@example.com");
+        webDriver.findElement(By.xpath("//input[@id='email']")).sendKeys("admin@example.com");
 
+        //password
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-        webDriver.findElement(By.id("password")).sendKeys("123456");
+        //webDriver.findElement(By.id("password")).sendKeys("123456");
+        webDriver.findElement(By.xpath("//input[@id='password']")).sendKeys("123456");
 
+        webDriver.findElement(By.xpath("//input[@id='remember']")).click();
 
-        webDriver.findElement(By.tagName("button")).click();
+        webDriver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
 
         Thread.sleep(3000);
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
